7777763f8bcbdcaee3ffda1594823f1936f9e5c7
chinonso Nwokonko <nwokonkochinonso@gmail.com>
2022-08-29 10:23:44 +0100

    multimodule project controller

--- web/src/main/java/com/watwithney/web/Controller.java
+++ web/src/main/java/com/watwithney/web/Controller.java
@@ -1,9 +1,11 @@
 package com.watwithney.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
     @RestController
+    @RequestMapping("controller")
     public class Controller {
 
         @GetMapping("/deploy")
